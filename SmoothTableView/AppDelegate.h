5c6a632cab360ed9c274524338b962927feda4b5
GuoLuchuan <guoluchuan@gmail.com>
2013-03-12 10:58:36 +0800

    creat project

---
+++ SmoothTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmoothTableView
+//
+//  Created by guo luchuan on 13-3-12.
+//  Copyright (c) 2013年 yours. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmoothTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmoothTableView
+//
+//  Created by guo luchuan on 13-3-12.
+//  Copyright (c) 2013年 yours. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
